ff98d9f8ac59788660e17c2e3550bd89ad126508
Gloria-HH <zhihuilvy@gmail.com>
2019-02-27 21:53:56 +0800

    fix code

--- src/test/java/com/demo/LoginTest.java
+++ src/test/java/com/demo/LoginTest.java
@@ -5,45 +5,49 @@ import org.testng.annotations.Test;
 
 import com.demo.flow.HomeFlow;
 import com.demo.flow.LoginFlow;
-import com.demo.page.BasePage;
-import com.demo.page.LoginPage;
 import com.demo.param.LoginParamter;
 
 public class LoginTest {
 
+	private HomeFlow homeFlow;
+
 	@Test
 	public void loginPass() {
-		HomeFlow homeFlow = new HomeFlow();
+		homeFlow = new HomeFlow();
 		LoginParamter loginParamter = new LoginParamter();
-		loginParamter.setUserName("XXX");
-		loginParamter.setPassword("XXX");
+		loginParamter.setUserName("testUseSele");
+		loginParamter.setPassword("!qwer1234!?");
 		LoginFlow loginFlow = new LoginFlow(loginParamter);
 		loginFlow.withStartPage(homeFlow.navigateToLogin()).execute();
-		
+		Assertions.assertThat("GitHub"
+				.equals(loginFlow.getEndPage().getTitle()));
+
 		loginFlow.close();
 	}
-	
+
 	@Test
 	public void loginFailureWithErrorUsername() {
-		HomeFlow homeFlow = new HomeFlow();
+		homeFlow = new HomeFlow();
 		LoginParamter loginParamter = new LoginParamter();
 		loginParamter.setUserName("XXX");
 		loginParamter.setPassword("XXX");
 		LoginFlow loginFlow = new LoginFlow(loginParamter);
 		loginFlow.withStartPage(homeFlow.navigateToLogin()).execute();
-		Assertions.assertThat(loginFlow.startPage.getErrorMessage()).isEqualTo("Incorrect username or password.");
+		Assertions.assertThat(loginFlow.startPage.getErrorMessage()).isEqualTo(
+				"Incorrect username or password.");
 		loginFlow.close();
 	}
-	
+
 	@Test
 	public void loginFailureWithErrorPassword() {
-		HomeFlow homeFlow = new HomeFlow();
+		homeFlow = new HomeFlow();
 		LoginParamter loginParamter = new LoginParamter();
 		loginParamter.setUserName("XXX");
 		loginParamter.setPassword("XXX");
 		LoginFlow loginFlow = new LoginFlow(loginParamter);
 		loginFlow.withStartPage(homeFlow.navigateToLogin()).execute();
-		Assertions.assertThat(loginFlow.startPage.getErrorMessage()).isEqualTo("Incorrect username or password.");
+		Assertions.assertThat(loginFlow.startPage.getErrorMessage()).isEqualTo(
+				"Incorrect username or password.");
 		loginFlow.close();
 	}
 
